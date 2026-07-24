018182cd460995b36cfc8977eb7dd956ba55237c
nakutakaa <nakutakaa@gmail.com>
2026-07-24 21:56:32 +0300

    image tesing and geolocation testing

--- simplelet-client/vite.config.js
+++ simplelet-client/vite.config.js
@@ -1,14 +1,22 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
+    },
+    host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000", // Backend is HTTP (not HTTPS)
         changeOrigin: true,
+        secure: false, // Allow self-signed certificates
       },
     },
   },
